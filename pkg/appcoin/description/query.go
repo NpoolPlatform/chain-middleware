0b831b2995c044208e1a72311b123d7a5d2ca444
Zhao KK <kikakkz@hotmail.com>
2022-11-08 12:57:14 +0000

    Support to create

--- pkg/appcoin/description/query.go
+++ pkg/appcoin/description/query.go
@@ -1,1 +1,11 @@
 package description
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/appcoin/description"
+)
+
+func GetCoinDescription(ctx context.Context, id string) (*npool.CoinDescription, error) {
+	return nil, nil
+}
