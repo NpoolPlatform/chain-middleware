4e16a5b130a78bae6ffb76da0893a3aaa865130d
Zhao KK <kikakkz@hotmail.com>
2022-11-11 05:59:36 +0000

    Add description UT

---
+++ pkg/client/appcoin/description/client.go
@@ -0,0 +1,105 @@
+package description
+
+import (
+	"context"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	descmgrpb "github.com/NpoolPlatform/message/npool/chain/mgr/v1/appcoin/description"
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/appcoin/description"
+
+	constant "github.com/NpoolPlatform/chain-middleware/pkg/message/const"
+)
+
+var timeout = 10 * time.Second
+
+type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
+
+func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(constant.ServiceName, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	cli := npool.NewMiddlewareClient(conn)
+
+	return handler(_ctx, cli)
+}
+
+func CreateCoinDescription(ctx context.Context, in *descmgrpb.CoinDescriptionReq) (*npool.CoinDescription, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateCoinDescription(ctx, &npool.CreateCoinDescriptionRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.CoinDescription), nil
+}
+
+func GetCoinDescription(ctx context.Context, id string) (*npool.CoinDescription, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetCoinDescription(ctx, &npool.GetCoinDescriptionRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.CoinDescription), nil
+}
+
+func GetCoinDescriptions(ctx context.Context, conds *descmgrpb.Conds, offset, limit int32) ([]*npool.CoinDescription, uint32, error) {
+	var total uint32
+
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetCoinDescriptions(ctx, &npool.GetCoinDescriptionsRequest{
+			Conds:  conds,
+			Offset: offset,
+			Limit:  limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		total = resp.Total
+
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return infos.([]*npool.CoinDescription), total, nil
+}
+
+func UpdateCoinDescription(ctx context.Context, in *descmgrpb.CoinDescriptionReq) (*npool.CoinDescription, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.UpdateCoinDescription(ctx, &npool.UpdateCoinDescriptionRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.CoinDescription), nil
+}
