ec1dfc362f54273a100668a888f5ade0662c4a2f
Zhao KK <kikakkz@hotmail.com>
2022-11-18 19:10:51 +0000

    Add currency feed apis

--- pkg/coin/currency/feed/delete.go
+++ pkg/coin/currency/feed/delete.go
@@ -1,1 +1,22 @@
 package currencyfeed
+
+import (
+	"context"
+
+	feedmgrcli "github.com/NpoolPlatform/chain-manager/pkg/client/coin/currency/feed"
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/coin/currency/feed"
+)
+
+func DeleteCurrencyFeed(ctx context.Context, id string) (*npool.CurrencyFeed, error) {
+	info, err := GetCurrencyFeed(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = feedmgrcli.DeleteCurrencyFeed(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return info, nil
+}
--- pkg/coin/currency/feed/update.go
+++ pkg/coin/currency/feed/update.go
@@ -1,1 +1,19 @@
 package currencyfeed
+
+import (
+	"context"
+
+	feedmgrpb "github.com/NpoolPlatform/message/npool/chain/mgr/v1/coin/currency/feed"
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/coin/currency/feed"
+
+	feedmgrcli "github.com/NpoolPlatform/chain-manager/pkg/client/coin/currency/feed"
+)
+
+func UpdateCurrencyFeed(ctx context.Context, in *feedmgrpb.CurrencyFeedReq) (*npool.CurrencyFeed, error) {
+	info, err := feedmgrcli.UpdateCurrencyFeed(ctx, in)
+	if err != nil {
+		return nil, err
+	}
+
+	return GetCurrencyFeed(ctx, info.ID)
+}
