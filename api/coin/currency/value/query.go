98a8bbd02fa675fadb425e2e5555f4d4b8ccdbd1
Zhao KK <kikakkz@hotmail.com>
2022-11-18 19:37:30 +0000

    Add currency api

--- api/coin/currency/value/query.go
+++ api/coin/currency/value/query.go
@@ -1,1 +1,23 @@
 package currencyvalue
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/coin/currency/value"
+)
+
+func (s *Server) GetCurrency(ctx context.Context, in *npool.GetCurrencyRequest) (*npool.GetCurrencyResponse, error) {
+	return nil, nil
+}
+
+func (s *Server) GetCoinCurrency(ctx context.Context, in *npool.GetCoinCurrencyRequest) (*npool.GetCoinCurrencyResponse, error) {
+	return nil, nil
+}
+
+func (s *Server) GetCurrencies(ctx context.Context, in *npool.GetCurrenciesRequest) (*npool.GetCurrenciesResponse, error) {
+	return nil, nil
+}
+
+func (s *Server) GetHistories(ctx context.Context, in *npool.GetHistoriesRequest) (*npool.GetHistoriesResponse, error) {
+	return nil, nil
+}
