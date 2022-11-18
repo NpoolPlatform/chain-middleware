d51ab41d33f5a777463f08bc1fd84d9691640605
Zhao KK <kikakkz@hotmail.com>
2022-11-18 16:51:44 +0000

    Add create and query

--- pkg/coin/currency/feed/query.go
+++ pkg/coin/currency/feed/query.go
@@ -1,1 +1,11 @@
 package currencyfeed
+
+import (
+	"context"
+
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/coin/currency/feed"
+)
+
+func GetCurrencyFeed(ctx context.Context, id string) (*npool.CurrencyFeed, error) {
+	return nil, nil
+}
