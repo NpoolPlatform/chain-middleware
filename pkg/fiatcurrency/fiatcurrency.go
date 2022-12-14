bfe4e233d126f540be463c831a71cec2de9b3610
fxnt <1330672764@qq.com>
2022-12-14 16:57:17 +0800

    update fiat

--- pkg/fiatcurrency/fiatcurrency.go
+++ pkg/fiatcurrency/fiatcurrency.go
@@ -12,36 +12,10 @@ import (
 	"github.com/shopspring/decimal"
 )
 
-func stableUSD(coinName string) bool {
-	priceMap := map[string]string{
-		"tusdt":       "tether",
-		"usdt":        "tether",
-		"tusdterc20":  "tether",
-		"usdterc20":   "tether",
-		"tusdttrc20":  "tether",
-		"usdttrc20":   "tether",
-		"tbinanceusd": "binance-usd",
-		"binanceusd":  "binance-usd",
-		"tusdcerc20":  "usdc",
-		"usdcerc20":   "usdc",
-	}
-	_, ok := priceMap[coinName]
-	return ok
-}
-
-func CoinUSDPrices(ctx context.Context, coinNames []string) (map[string]decimal.Decimal, currencymgrpb.FeedType, error) {
+func CoinUSDPrices(ctx context.Context, fiatCurrencyNames []string) (map[string]decimal.Decimal, currencymgrpb.FeedType, error) {
 	prices := map[string]decimal.Decimal{}
-	names := []string{}
 
-	for _, name := range coinNames {
-		if stableUSD(name) {
-			prices[name] = decimal.NewFromInt(1)
-			continue
-		}
-		names = append(names, name)
-	}
-
-	prices1, err := coingecko.CoinGeckoFiatCurrency(names)
+	prices1, err := coingecko.UsdFiatCurrency(fiatCurrencyNames)
 	if err == nil {
 		for name, price := range prices1 {
 			prices[name] = price
@@ -51,7 +25,7 @@ func CoinUSDPrices(ctx context.Context, coinNames []string) (map[string]decimal.
 
 	logger.Sugar().Errorw("CoinUSDPrices", "Feed", "CoinGecko", "error", err)
 
-	prices1, err = coinbase.CoinBaseUSDPrices(names)
+	prices1, err = coinbase.UsdFaitCurrency()
 	if err != nil {
 		logger.Sugar().Errorw("CoinUSDPrices", "Feed", "CoinBase", "error", err)
 		return nil, currencymgrpb.FeedType_DefaultFeedType, err
