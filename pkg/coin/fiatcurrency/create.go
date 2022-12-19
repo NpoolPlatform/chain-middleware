847276af55c4583e1702090ccdcb7812a1d587df
JobNing <q178149@163.com>
2022-12-19 10:46:37 +0800

    update create resp

--- pkg/coin/fiatcurrency/create.go
+++ pkg/coin/fiatcurrency/create.go
@@ -3,10 +3,14 @@ package fiatcurrency
 import (
 	"context"
 
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+
 	fiatcurrencymgrcli "github.com/NpoolPlatform/chain-manager/pkg/client/coin/fiatcurrency"
 	fiatcurrencymgrpb "github.com/NpoolPlatform/message/npool/chain/mgr/v1/coin/fiatcurrency"
 
 	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/coin/fiatcurrency"
+
+	npoolpb "github.com/NpoolPlatform/message/npool"
 )
 
 func CreateFiatCurrency(ctx context.Context, in *fiatcurrencymgrpb.FiatCurrencyReq) (*npool.FiatCurrency, error) {
@@ -14,14 +18,22 @@ func CreateFiatCurrency(ctx context.Context, in *fiatcurrencymgrpb.FiatCurrencyR
 	if err != nil {
 		return nil, err
 	}
-	return nil, err
+	return GetFiatCurrency(ctx, in.GetFiatCurrencyTypeID())
 }
 
 func CreateFiatCurrencies(ctx context.Context, in []*fiatcurrencymgrpb.FiatCurrencyReq) ([]*npool.FiatCurrency, error) {
-	_, err := fiatcurrencymgrcli.CreateFiatCurrencies(ctx, in)
+	infos, err := fiatcurrencymgrcli.CreateFiatCurrencies(ctx, in)
 	if err != nil {
 		return nil, err
 	}
-
-	return nil, err
+	ids := []string{}
+	for _, val := range infos {
+		ids = append(ids, val.FiatCurrencyTypeID)
+	}
+	return GetFiatCurrencies(ctx, &npool.Conds{
+		FiatCurrencyTypeIDs: &npoolpb.StringSliceVal{
+			Op:    cruder.IN,
+			Value: ids,
+		},
+	})
 }
