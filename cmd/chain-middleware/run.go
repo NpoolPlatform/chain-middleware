459b1432b5b9cadb1f9f5f7555434f845a236bd7
JobNing <q178149@163.com>
2023-01-11 18:08:59 +0800

    update register api

--- cmd/chain-middleware/run.go
+++ cmd/chain-middleware/run.go
@@ -7,7 +7,7 @@ import (
 	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
 
-	apimgrcli "github.com/NpoolPlatform/api-manager/pkg/client"
+	apicli "github.com/NpoolPlatform/basal-middleware/pkg/client/api"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 
@@ -40,7 +40,7 @@ var runCmd = &cli.Command{
 func rpcRegister(server grpc.ServiceRegistrar) error {
 	api.Register(server)
 
-	apimgrcli.RegisterGRPC(server)
+	apicli.RegisterGRPC(server)
 
 	return nil
 }
@@ -51,7 +51,7 @@ func rpcGatewayRegister(mux *runtime.ServeMux, endpoint string, opts []grpc.Dial
 		return err
 	}
 
-	apimgrcli.Register(mux) //nolint
+	_ = apicli.Register(mux)
 
 	return nil
 }
