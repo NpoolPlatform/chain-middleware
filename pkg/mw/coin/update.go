987d4505c2474a7c2ecc22159e5e27e5e270661f
Zhao KK <kikakkz@hotmail.com>
2023-05-23 09:57:59 +0000

    Support update

--- pkg/mw/coin/update.go
+++ pkg/mw/coin/update.go
@@ -2,88 +2,172 @@ package coin
 
 import (
 	"context"
+	"fmt"
 
 	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/coin"
 
-	basecrud "github.com/NpoolPlatform/chain-manager/pkg/crud/coin/base"
-	extracrud "github.com/NpoolPlatform/chain-manager/pkg/crud/coin/extra"
-	settingcrud "github.com/NpoolPlatform/chain-manager/pkg/crud/coin/setting"
-	basemgrpb "github.com/NpoolPlatform/message/npool/chain/mgr/v1/coin/base"
-	extramgrpb "github.com/NpoolPlatform/message/npool/chain/mgr/v1/coin/extra"
-	settingmgrpb "github.com/NpoolPlatform/message/npool/chain/mgr/v1/coin/setting"
+	basecrud "github.com/NpoolPlatform/chain-middleware/pkg/crud/coin"
+	extracrud "github.com/NpoolPlatform/chain-middleware/pkg/crud/coin/extra"
+	settingcrud "github.com/NpoolPlatform/chain-middleware/pkg/crud/coin/setting"
 
-	"github.com/NpoolPlatform/chain-manager/pkg/db"
-	"github.com/NpoolPlatform/chain-manager/pkg/db/ent"
-
-	entbase "github.com/NpoolPlatform/chain-manager/pkg/db/ent/coinbase"
-	entextra "github.com/NpoolPlatform/chain-manager/pkg/db/ent/coinextra"
-	entsetting "github.com/NpoolPlatform/chain-manager/pkg/db/ent/setting"
-
-	"github.com/google/uuid"
+	"github.com/NpoolPlatform/chain-middleware/pkg/db"
+	"github.com/NpoolPlatform/chain-middleware/pkg/db/ent"
+	entextra "github.com/NpoolPlatform/chain-middleware/pkg/db/ent/coinextra"
+	entsetting "github.com/NpoolPlatform/chain-middleware/pkg/db/ent/setting"
 )
 
-func UpdateCoin(ctx context.Context, in *npool.CoinReq) (*npool.Coin, error) {
-	var err error
+type updateHandler struct {
+	*Handler
+}
 
-	err = db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
-		info, err := tx.CoinBase.Query().Where(entbase.ID(uuid.MustParse(in.GetID()))).ForUpdate().Only(_ctx)
-		if err != nil {
+func (h *updateHandler) updateCoinBase(ctx context.Context, tx *ent.Tx) error {
+	if _, err := basecrud.UpdateSet(
+		tx.CoinBase.UpdateOneID(*h.ID),
+		&basecrud.Req{
+			ID:             h.ID,
+			Name:           h.Name,
+			Logo:           h.Logo,
+			Presale:        h.Presale,
+			Unit:           h.Unit,
+			ENV:            h.ENV,
+			ReservedAmount: h.ReservedAmount,
+			ForPay:         h.ForPay,
+		},
+	).Save(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *updateHandler) updateCoinExtra(ctx context.Context, tx *ent.Tx) error {
+	info, err := tx.
+		CoinExtra.
+		Query().
+		Where(
+			entextra.CoinTypeID(*h.ID),
+		).
+		ForUpdate().
+		Only(ctx)
+	if err != nil {
+		if !ent.IsNotFound(err) {
 			return err
 		}
+	}
 
-		_, err = basecrud.UpdateSet(info, &basemgrpb.CoinBaseReq{
-			ID:             in.ID,
-			Logo:           in.Logo,
-			Presale:        in.Presale,
-			ReservedAmount: in.ReservedAmount,
-			ForPay:         in.ForPay,
-			Disabled:       in.Disabled,
-		}).Save(_ctx)
-		if err != nil {
+	if info != nil {
+		if _, err := extracrud.UpdateSet(
+			info.Update(),
+			&extracrud.Req{
+				CoinTypeID: h.ID,
+				HomePage:   h.HomePage,
+				Specs:      h.Specs,
+			},
+		).Save(ctx); err != nil {
 			return err
 		}
+		return nil
+	}
 
-		info1, err := tx.CoinExtra.Query().Where(entextra.CoinTypeID(uuid.MustParse(in.GetID()))).ForUpdate().Only(_ctx)
-		if err != nil {
+	if _, err := extracrud.CreateSet(
+		tx.CoinExtra.Create(),
+		&extracrud.Req{
+			CoinTypeID: h.ID,
+			HomePage:   h.HomePage,
+			Specs:      h.Specs,
+		},
+	).Save(ctx); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *updateHandler) updateCoinSetting(ctx context.Context, tx *ent.Tx) error {
+	info, err := tx.
+		Setting.
+		Query().
+		Where(
+			entsetting.CoinTypeID(*h.ID),
+		).
+		ForUpdate().
+		Only(ctx)
+	if err != nil {
+		if !ent.IsNotFound(err) {
 			return err
 		}
+	}
 
-		_, err = extracrud.UpdateSet(info1, &extramgrpb.CoinExtraReq{
-			HomePage:  in.HomePage,
-			Specs:     in.Specs,
-			StableUSD: in.StableUSD,
-		}).Save(_ctx)
-		if err != nil {
+	if info != nil {
+		if _, err := settingcrud.UpdateSet(
+			info.Update(),
+			&settingcrud.Req{
+				CoinTypeID:                  h.ID,
+				FeeCoinTypeID:               h.FeeCoinTypeID,
+				WithdrawFeeByStableUSD:      h.WithdrawFeeByStableUSD,
+				WithdrawFeeAmount:           h.WithdrawFeeAmount,
+				CollectFeeAmount:            h.CollectFeeAmount,
+				HotWalletFeeAmount:          h.HotWalletFeeAmount,
+				LowFeeAmount:                h.LowFeeAmount,
+				HotLowFeeAmount:             h.HotLowFeeAmount,
+				HotWalletAccountAmount:      h.HotWalletAccountAmount,
+				PaymentAccountCollectAmount: h.PaymentAccountCollectAmount,
+				LeastTransferAmount:         h.LeastTransferAmount,
+				NeedMemo:                    h.NeedMemo,
+				RefreshCurrency:             h.RefreshCurrency,
+			},
+		).Save(ctx); err != nil {
 			return err
 		}
+		return nil
+	}
 
-		info2, err := tx.Setting.Query().Where(entsetting.CoinTypeID(uuid.MustParse(in.GetID()))).ForUpdate().Only(_ctx)
-		if err != nil {
+	if _, err := settingcrud.CreateSet(
+		tx.Setting.Create(),
+		&settingcrud.Req{
+			CoinTypeID:                  h.ID,
+			FeeCoinTypeID:               h.FeeCoinTypeID,
+			WithdrawFeeByStableUSD:      h.WithdrawFeeByStableUSD,
+			WithdrawFeeAmount:           h.WithdrawFeeAmount,
+			CollectFeeAmount:            h.CollectFeeAmount,
+			HotWalletFeeAmount:          h.HotWalletFeeAmount,
+			LowFeeAmount:                h.LowFeeAmount,
+			HotLowFeeAmount:             h.HotLowFeeAmount,
+			HotWalletAccountAmount:      h.HotWalletAccountAmount,
+			PaymentAccountCollectAmount: h.PaymentAccountCollectAmount,
+			LeastTransferAmount:         h.LeastTransferAmount,
+			NeedMemo:                    h.NeedMemo,
+			RefreshCurrency:             h.RefreshCurrency,
+		},
+	).Save(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (h *Handler) UpdateCoin(ctx context.Context) (*npool.Coin, error) {
+	if h.ID == nil {
+		return nil, fmt.Errorf("invalid cointypeid")
+	}
+
+	handler := &updateHandler{
+		Handler: h,
+	}
+
+	err := db.WithTx(ctx, func(_ctx context.Context, tx *ent.Tx) error {
+		if err := handler.updateCoinBase(_ctx, tx); err != nil {
 			return err
 		}
-
-		_, err = settingcrud.UpdateSet(info2, &settingmgrpb.SettingReq{
-			FeeCoinTypeID:               in.FeeCoinTypeID,
-			WithdrawFeeByStableUSD:      in.WithdrawFeeByStableUSD,
-			WithdrawFeeAmount:           in.WithdrawFeeAmount,
-			CollectFeeAmount:            in.CollectFeeAmount,
-			HotWalletFeeAmount:          in.HotWalletFeeAmount,
-			LowFeeAmount:                in.LowFeeAmount,
-			HotLowFeeAmount:             in.HotLowFeeAmount,
-			HotWalletAccountAmount:      in.HotWalletAccountAmount,
-			PaymentAccountCollectAmount: in.PaymentAccountCollectAmount,
-			LeastTransferAmount:         in.LeastTransferAmount,
-			NeedMemo:                    in.NeedMemo,
-		}).Save(_ctx)
-		if err != nil {
+		if err := handler.updateCoinExtra(_ctx, tx); err != nil {
 			return err
 		}
-
+		if err := handler.updateCoinSetting(_ctx, tx); err != nil {
+			return err
+		}
 		return nil
 	})
 	if err != nil {
 		return nil, err
 	}
 
-	return GetCoin(ctx, in.GetID())
+	return h.GetCoin(ctx)
 }
