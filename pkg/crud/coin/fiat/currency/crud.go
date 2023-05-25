00435a0f75203747ab14ce1d1f4014b5da1ebe71
Zhao KK <kikakkz@hotmail.com>
2023-05-25 04:56:50 +0000

    Add coin fiat currency

---
+++ pkg/crud/coin/fiat/currency/crud.go
@@ -0,0 +1,86 @@
+package currency
+
+import (
+	"fmt"
+
+	"github.com/NpoolPlatform/chain-middleware/pkg/db/ent"
+	entcurrency "github.com/NpoolPlatform/chain-middleware/pkg/db/ent/coinfiatcurrency"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+)
+
+type Req struct {
+	CoinTypeID      *uuid.UUID
+	FiatID          *uuid.UUID
+	FeedType        *basetypes.CurrencyFeedType
+	MarketValueHigh *decimal.Decimal
+	MarketValueLow  *decimal.Decimal
+}
+
+func CreateSet(c *ent.CoinFiatCurrencyCreate, req *Req) *ent.CoinFiatCurrencyCreate {
+	if req.CoinTypeID != nil {
+		c.SetCoinTypeID(*req.CoinTypeID)
+	}
+	if req.FiatID != nil {
+		c.SetFiatID(*req.FiatID)
+	}
+	if req.FeedType != nil {
+		c.SetFeedType(req.FeedType.String())
+	}
+	if req.MarketValueHigh != nil {
+		c.SetMarketValueHigh(*req.MarketValueHigh)
+	}
+	if req.MarketValueLow != nil {
+		c.SetMarketValueLow(*req.MarketValueLow)
+	}
+	return c
+}
+
+func UpdateSet(u *ent.CoinFiatCurrencyUpdateOne, req *Req) *ent.CoinFiatCurrencyUpdateOne {
+	if req.MarketValueHigh != nil {
+		u = u.SetMarketValueHigh(*req.MarketValueHigh)
+	}
+	if req.MarketValueLow != nil {
+		u = u.SetMarketValueLow(*req.MarketValueLow)
+	}
+
+	return u
+}
+
+type Conds struct {
+	ID          *cruder.Cond
+	CoinTypeID  *cruder.Cond
+	CoinTypeIDs *cruder.Cond
+}
+
+func SetQueryConds(q *ent.CoinFiatCurrencyQuery, conds *Conds) (*ent.CoinFiatCurrencyQuery, error) {
+	if conds.CoinTypeID != nil {
+		id, ok := conds.CoinTypeID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid cointypeid")
+		}
+		switch conds.CoinTypeID.Op {
+		case cruder.EQ:
+			q.Where(entcurrency.CoinTypeID(id))
+		default:
+			return nil, fmt.Errorf("invalid currency field")
+		}
+	}
+	if conds.CoinTypeIDs != nil {
+		ids, ok := conds.CoinTypeIDs.Val.([]uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid cointypeids")
+		}
+		switch conds.CoinTypeIDs.Op {
+		case cruder.EQ:
+			q.Where(entcurrency.CoinTypeIDIn(ids...))
+		default:
+			return nil, fmt.Errorf("invalid currency field")
+		}
+	}
+	q.Where(entcurrency.DeletedAt(0))
+	return q, nil
+}
