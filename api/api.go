d0eae75b20687afe80bc521090d7d3fe3b07fc70
Zhao KK <kikakkz@hotmail.com>
2023-05-25 08:48:16 +0000

    Add coin fiat currency apis

--- api/api.go
+++ api/api.go
@@ -3,14 +3,17 @@ package api
 import (
 	"context"
 
-	"github.com/NpoolPlatform/chain-middleware/api/fiat"
-
 	chainmw "github.com/NpoolPlatform/message/npool/chain/mw/v1"
 
-	"github.com/NpoolPlatform/chain-middleware/api/appcoin"
-	"github.com/NpoolPlatform/chain-middleware/api/appcoin/description"
+	appcoin "github.com/NpoolPlatform/chain-middleware/api/app/coin"
+	"github.com/NpoolPlatform/chain-middleware/api/app/coin/description"
 	"github.com/NpoolPlatform/chain-middleware/api/coin"
-	currency "github.com/NpoolPlatform/chain-middleware/api/coin/currency"
+	coincurrency "github.com/NpoolPlatform/chain-middleware/api/coin/currency"
+	coincurrencyhis "github.com/NpoolPlatform/chain-middleware/api/coin/currency/history"
+	coinfiatcurrencyhis "github.com/NpoolPlatform/chain-middleware/api/coin/fiat/currency/history"
+	"github.com/NpoolPlatform/chain-middleware/api/fiat"
+	fiatcurrency "github.com/NpoolPlatform/chain-middleware/api/fiat/currency"
+	fiatcurrencyhis "github.com/NpoolPlatform/chain-middleware/api/fiat/currency/history"
 	tran "github.com/NpoolPlatform/chain-middleware/api/tx"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
@@ -26,9 +29,13 @@ func Register(server grpc.ServiceRegistrar) {
 	appcoin.Register(server)
 	description.Register(server)
 	coin.Register(server)
-	tran.Register(server)
-	currency.Register(server)
+	coincurrency.Register(server)
+	coincurrencyhis.Register(server)
+	coinfiatcurrencyhis.Register(server)
 	fiat.Register(server)
+	fiatcurrency.Register(server)
+	fiatcurrencyhis.Register(server)
+	tran.Register(server)
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
