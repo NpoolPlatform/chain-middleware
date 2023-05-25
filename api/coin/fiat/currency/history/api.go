538529bd364b573671618e04096bb53d5d180494
Zhao KK <kikakkz@hotmail.com>
2023-05-25 08:00:32 +0000

    Add coin fiat currency history api

--- api/coin/fiat/currency/history/api.go
+++ api/coin/fiat/currency/history/api.go
@@ -1,18 +1,18 @@
-package description
+package currencyhistory
 
 import (
-	"github.com/NpoolPlatform/message/npool/chain/mw/v1/appcoin/description"
+	"github.com/NpoolPlatform/message/npool/chain/mw/v1/coin/fiat/currency/history"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 )
 
 type Server struct {
-	description.UnimplementedMiddlewareServer
+	history.UnimplementedMiddlewareServer
 }
 
 func Register(server grpc.ServiceRegistrar) {
-	description.RegisterMiddlewareServer(server, &Server{})
+	history.RegisterMiddlewareServer(server, &Server{})
 }
 
 func RegisterGateway(mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) error {
