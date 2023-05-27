8f4667f2c0e18f4363239d3748d7d895fc1fa4b4
Zhao KK <kikakkz@hotmail.com>
2023-05-27 03:47:06 +0000

    Add app coin description client api

---
+++ pkg/client/app/coin/description/client.go
@@ -0,0 +1,104 @@
+package description
+
+import (
+	"context"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/app/coin/description"
+
+	servicename "github.com/NpoolPlatform/chain-middleware/pkg/servicename"
+)
+
+var timeout = 10 * time.Second
+
+type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
+
+func do(ctx context.Context, handler handler) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	cli := npool.NewMiddlewareClient(conn)
+
+	return handler(_ctx, cli)
+}
+
+func CreateCoinDescription(ctx context.Context, in *npool.CoinDescriptionReq) (*npool.CoinDescription, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateCoinDescription(ctx, &npool.CreateCoinDescriptionRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.CoinDescription), nil
+}
+
+func GetCoinDescription(ctx context.Context, id string) (*npool.CoinDescription, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetCoinDescription(ctx, &npool.GetCoinDescriptionRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.CoinDescription), nil
+}
+
+func GetCoinDescriptions(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.CoinDescription, uint32, error) {
+	var total uint32
+
+	infos, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetCoinDescriptions(ctx, &npool.GetCoinDescriptionsRequest{
+			Conds:  conds,
+			Offset: offset,
+			Limit:  limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		total = resp.Total
+
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return infos.([]*npool.CoinDescription), total, nil
+}
+
+func UpdateCoinDescription(ctx context.Context, in *npool.CoinDescriptionReq) (*npool.CoinDescription, error) {
+	info, err := do(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.UpdateCoinDescription(ctx, &npool.UpdateCoinDescriptionRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.CoinDescription), nil
+}
---
+++ pkg/client/app/coin/description/client_test.go
@@ -0,0 +1,157 @@
+package description
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/NpoolPlatform/chain-middleware/pkg/testinit"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/config"
+
+	"bou.ke/monkey"
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/app/coin/description"
+	"github.com/stretchr/testify/assert"
+
+	appcoin1 "github.com/NpoolPlatform/chain-middleware/pkg/mw/app/coin"
+	coin1 "github.com/NpoolPlatform/chain-middleware/pkg/mw/coin"
+
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+
+	"github.com/google/uuid"
+)
+
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+}
+
+var ret = &npool.CoinDescription{
+	ID:         uuid.NewString(),
+	AppID:      uuid.NewString(),
+	CoinLogo:   uuid.NewString(),
+	CoinUnit:   "BTC",
+	CoinENV:    "test",
+	UsedForStr: basetypes.UsedFor_ProductPage.String(),
+	UsedFor:    basetypes.UsedFor_ProductPage,
+	Title:      uuid.NewString(),
+	Message:    uuid.NewString(),
+}
+
+var req = &npool.CoinDescriptionReq{
+	AppID:   &ret.AppID,
+	UsedFor: &ret.UsedFor,
+	Title:   &ret.Title,
+	Message: &ret.Message,
+}
+
+func setupAppCoinDescription(t *testing.T) func(*testing.T) {
+	ret.CoinTypeID = uuid.NewString()
+	req.CoinTypeID = &ret.CoinTypeID
+	ret.CoinName = uuid.NewString()
+
+	h1, err := coin1.NewHandler(
+		context.Background(),
+		coin1.WithID(&ret.CoinTypeID),
+		coin1.WithName(&ret.CoinName),
+		coin1.WithLogo(&ret.CoinLogo),
+		coin1.WithUnit(&ret.CoinUnit),
+		coin1.WithENV(&ret.CoinENV),
+	)
+	assert.Nil(t, err)
+
+	_, err = h1.CreateCoin(context.Background())
+	assert.Nil(t, err)
+
+	h2, err := appcoin1.NewHandler(
+		context.Background(),
+		appcoin1.WithAppID(&ret.AppID),
+		appcoin1.WithCoinTypeID(&ret.CoinTypeID),
+	)
+	assert.Nil(t, err)
+
+	_, err = h2.CreateCoin(context.Background())
+	assert.Nil(t, err)
+
+	return func(*testing.T) {
+		_, _ = h1.DeleteCoin(context.Background())
+		_, _ = h2.DeleteCoin(context.Background())
+	}
+}
+
+func createCoinDescription(t *testing.T) {
+	info, err := CreateCoinDescription(context.Background(), req)
+	if assert.Nil(t, err) {
+		ret.CreatedAt = info.CreatedAt
+		ret.UpdatedAt = info.UpdatedAt
+		ret.ID = info.ID
+		assert.Equal(t, ret, info)
+	}
+}
+
+func updateCoinDescription(t *testing.T) {
+	ret.Title = uuid.NewString()
+	ret.Message = uuid.NewString()
+
+	req.ID = &ret.ID
+	req.Title = &ret.Title
+	req.Message = &ret.Message
+
+	info, err := UpdateCoinDescription(context.Background(), req)
+	if assert.Nil(t, err) {
+		ret.UpdatedAt = info.UpdatedAt
+		assert.Equal(t, info, ret)
+	}
+}
+
+func getCoinDescription(t *testing.T) {
+	info, err := GetCoinDescription(context.Background(), ret.ID)
+	if assert.Nil(t, err) {
+		assert.Equal(t, info, ret)
+	}
+}
+
+func getCoinDescriptions(t *testing.T) {
+	infos, total, err := GetCoinDescriptions(context.Background(), &npool.Conds{
+		ID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.ID},
+	}, 0, 100)
+	if assert.Nil(t, err) {
+		assert.Equal(t, len(infos), 1)
+		assert.Equal(t, total, uint32(1))
+		if assert.Equal(t, len(infos), 1) {
+			assert.Equal(t, infos[0], ret)
+		}
+	}
+}
+
+func TestClient(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	// Here won't pass test due to we always test with localhost
+
+	teardown := setupAppCoinDescription(t)
+	defer teardown(t)
+
+	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
+
+	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
+		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	})
+
+	t.Run("createCoinDescription", createCoinDescription)
+	t.Run("updateCoinDescription", updateCoinDescription)
+	t.Run("getCoinDescription", getCoinDescription)
+	t.Run("getCoinDescriptions", getCoinDescriptions)
+}
