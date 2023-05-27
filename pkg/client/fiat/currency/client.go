5a9bb32638bad6cdf76924dfb3b692f37d93d37f
Zhao KK <kikakkz@hotmail.com>
2023-05-27 03:19:04 +0000

    Add fiat client

---
+++ pkg/client/fiat/currency/client.go
@@ -0,0 +1,72 @@
+package currency
+
+import (
+	"context"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/fiat/currency"
+
+	servicename "github.com/NpoolPlatform/chain-middleware/pkg/servicename"
+)
+
+var timeout = 10 * time.Second
+
+type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
+
+func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	cli := npool.NewMiddlewareClient(conn)
+
+	return handler(_ctx, cli)
+}
+
+func GetCurrency(ctx context.Context, id string) (*npool.Currency, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetCurrency(ctx, &npool.GetCurrencyRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Currency), nil
+}
+
+func GetCurrencies(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.Currency, uint32, error) {
+	total := uint32(0)
+
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetCurrencies(ctx, &npool.GetCurrenciesRequest{
+			Conds:  conds,
+			Offset: offset,
+			Limit:  limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		total = resp.Total
+
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return infos.([]*npool.Currency), total, nil
+}
---
+++ pkg/client/fiat/currency/client_test.go
@@ -0,0 +1,126 @@
+package currency
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/NpoolPlatform/chain-middleware/pkg/testinit"
+
+	"github.com/NpoolPlatform/go-service-framework/pkg/config"
+
+	"bou.ke/monkey"
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	fiat1 "github.com/NpoolPlatform/chain-middleware/pkg/mw/fiat"
+	currency1 "github.com/NpoolPlatform/chain-middleware/pkg/mw/fiat/currency"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/fiat/currency"
+	"github.com/stretchr/testify/assert"
+
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+
+	"github.com/google/uuid"
+)
+
+func init() {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	if err := testinit.Init(); err != nil {
+		fmt.Printf("cannot init test stub: %v\n", err)
+	}
+}
+
+var ret = &npool.Currency{
+	ID:              uuid.NewString(),
+	FiatName:        uuid.NewString(),
+	FiatUnit:        uuid.NewString(),
+	FeedType:        basetypes.CurrencyFeedType_CoinBase,
+	FeedTypeStr:     basetypes.CurrencyFeedType_CoinBase.String(),
+	MarketValueHigh: "12.001000000000000000",
+	MarketValueLow:  "11.001000000000000000",
+}
+
+var req = &npool.CurrencyReq{
+	ID:              &ret.ID,
+	FeedType:        &ret.FeedType,
+	MarketValueHigh: &ret.MarketValueHigh,
+	MarketValueLow:  &ret.MarketValueLow,
+}
+
+func setupTx(t *testing.T) func(*testing.T) {
+	ret.FiatID = uuid.NewString()
+	req.FiatID = &ret.FiatID
+
+	h1, err := fiat1.NewHandler(
+		context.Background(),
+		fiat1.WithID(&ret.FiatID),
+		fiat1.WithName(&ret.FiatName),
+		fiat1.WithUnit(&ret.FiatUnit),
+		fiat1.WithLogo(&ret.FiatLogo),
+	)
+	assert.Nil(t, err)
+
+	_, err = h1.CreateFiat(context.Background())
+	assert.Nil(t, err)
+
+	h2, err := currency1.NewHandler(
+		context.Background(),
+		currency1.WithFiatID(req.FiatID),
+		currency1.WithMarketValueHigh(req.MarketValueHigh),
+		currency1.WithMarketValueLow(req.MarketValueLow),
+		currency1.WithFeedType(req.FeedType),
+	)
+	assert.Nil(t, err)
+
+	info, err := h2.CreateCurrency(context.Background())
+	assert.Nil(t, err)
+
+	ret.ID = info.ID
+	ret.CreatedAt = info.CreatedAt
+	ret.UpdatedAt = info.UpdatedAt
+
+	return func(*testing.T) {}
+}
+
+func getCurrency(t *testing.T) {
+	info, err := GetCurrency(context.Background(), ret.ID)
+	if assert.Nil(t, err) {
+		assert.Equal(t, ret, info)
+	}
+}
+
+func getCurrencies(t *testing.T) {
+	infos, total, err := GetCurrencies(context.Background(), &npool.Conds{
+		FiatID: &basetypes.StringVal{Op: cruder.EQ, Value: ret.FiatID},
+	}, 0, 100)
+	if assert.Nil(t, err) {
+		assert.Equal(t, 1, len(infos))
+		assert.Equal(t, uint32(1), total)
+		assert.Equal(t, ret, infos[0])
+	}
+}
+
+func TestClient(t *testing.T) {
+	if runByGithubAction, err := strconv.ParseBool(os.Getenv("RUN_BY_GITHUB_ACTION")); err == nil && runByGithubAction {
+		return
+	}
+	// Here won't pass test due to we always test with localhost
+
+	teardown := setupTx(t)
+	defer teardown(t)
+
+	gport := config.GetIntValueWithNameSpace("", config.KeyGRPCPort)
+
+	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
+		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	})
+
+	t.Run("getCurrency", getCurrency)
+	t.Run("getCurrencies", getCurrencies)
+}
