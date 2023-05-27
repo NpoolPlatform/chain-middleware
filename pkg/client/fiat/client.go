a0399fbe8178f6a3e65916c6e5d4a8327ef354f6
Zhao KK <kikakkz@hotmail.com>
2023-05-27 03:25:02 +0000

    Add client fiat UT

---
+++ pkg/client/fiat/client.go
@@ -0,0 +1,104 @@
+package fiat
+
+import (
+	"context"
+	"time"
+
+	grpc2 "github.com/NpoolPlatform/go-service-framework/pkg/grpc"
+
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	npool "github.com/NpoolPlatform/message/npool/chain/mw/v1/fiat"
+
+	servicename "github.com/NpoolPlatform/chain-middleware/pkg/servicename"
+)
+
+var timeout = 10 * time.Second
+
+type handler func(context.Context, npool.MiddlewareClient) (cruder.Any, error)
+
+func withCRUD(ctx context.Context, handler handler) (cruder.Any, error) {
+	_ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	conn, err := grpc2.GetGRPCConn(servicename.ServiceDomain, grpc2.GRPCTAG)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.Close()
+
+	cli := npool.NewMiddlewareClient(conn)
+
+	return handler(_ctx, cli)
+}
+
+func CreateFiat(ctx context.Context, in *npool.FiatReq) (*npool.Fiat, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.CreateFiat(ctx, &npool.CreateFiatRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Fiat), nil
+}
+
+func GetFiat(ctx context.Context, id string) (*npool.Fiat, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetFiat(ctx, &npool.GetFiatRequest{
+			ID: id,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Fiat), nil
+}
+
+func GetFiats(ctx context.Context, conds *npool.Conds, offset, limit int32) ([]*npool.Fiat, uint32, error) {
+	var total uint32
+
+	infos, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.GetFiats(ctx, &npool.GetFiatsRequest{
+			Conds:  conds,
+			Offset: offset,
+			Limit:  limit,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		total = resp.Total
+
+		return resp.Infos, nil
+	})
+	if err != nil {
+		return nil, 0, err
+	}
+	return infos.([]*npool.Fiat), total, nil
+}
+
+func UpdateFiat(ctx context.Context, in *npool.FiatReq) (*npool.Fiat, error) {
+	info, err := withCRUD(ctx, func(_ctx context.Context, cli npool.MiddlewareClient) (cruder.Any, error) {
+		resp, err := cli.UpdateFiat(ctx, &npool.UpdateFiatRequest{
+			Info: in,
+		})
+		if err != nil {
+			return nil, err
+		}
+		return resp.Info, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return info.(*npool.Fiat), nil
+}
