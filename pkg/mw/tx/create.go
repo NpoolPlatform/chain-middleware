82fd8f560b3762f69520d640ba90f54d7c5b96bc
Zhao KK <kikakkz@hotmail.com>
2023-06-05 14:21:07 +0000

    With right tx op

--- pkg/mw/tx/create.go
+++ pkg/mw/tx/create.go
@@ -64,7 +64,7 @@ func (h *Handler) CreateTxs(ctx context.Context) ([]*npool.Tx, error) {
 	}
 
 	h.Conds = &txcrud.Conds{
-		IDs: &cruder.Cond{Op: cruder.EQ, Val: ids},
+		IDs: &cruder.Cond{Op: cruder.IN, Val: ids},
 	}
 	h.Offset = 0
 	h.Limit = int32(len(ids))
