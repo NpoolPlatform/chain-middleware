ff4d86d1dbc7c55c6abeae04e7344cfbc18a072e
Zhao KK <kikakkz@hotmail.com>
2023-08-13 06:30:17 +0000

    update ut

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	bou.ke/monkey v1.0.2
 	entgo.io/ent v0.11.2
 	github.com/NpoolPlatform/basal-middleware v0.0.0-20230518061816-62bd589c0f63
-	github.com/NpoolPlatform/go-service-framework v0.0.0-20230813053445-1f5c88e41f5c
+	github.com/NpoolPlatform/go-service-framework v0.0.0-20230813055757-23d6ad3123fc
 	github.com/NpoolPlatform/libent-cruder v0.0.0-20221109023947-366125b690fb
 	github.com/NpoolPlatform/message v0.0.0-20230712034839-a249d6fa0e26
 	github.com/go-resty/resty/v2 v2.7.0
--- pkg/client/coin/currency/client_test.go
+++ pkg/client/coin/currency/client_test.go
@@ -124,6 +124,9 @@ func TestClient(t *testing.T) {
 	monkey.Patch(grpc2.GetGRPCConn, func(service string, tags ...string) (*grpc.ClientConn, error) {
 		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	})
+	monkey.Patch(grpc2.GetGRPCConnV1, func(service string, recvMsgBytes int, tags ...string) (*grpc.ClientConn, error) {
+		return grpc.Dial(fmt.Sprintf("localhost:%v", gport), grpc.WithTransportCredentials(insecure.NewCredentials()))
+	})
 
 	t.Run("getCurrency", getCurrency)
 	t.Run("getCurrencies", getCurrencies)
