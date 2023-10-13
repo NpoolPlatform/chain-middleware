898d39a1213dbf6faba61d3c778812688d6b1f24
jakys <jakysing@163.com>
2023-10-13 03:15:09 +0000

    Fix ut migration

--- pkg/testinit/testinit.go
+++ pkg/testinit/testinit.go
@@ -1,6 +1,7 @@
 package testinit
 
 import (
+	"context"
 	"fmt"
 	"path"
 	"runtime"
@@ -9,6 +10,7 @@ import (
 
 	"github.com/NpoolPlatform/chain-middleware/pkg/db"
 
+	migrator "github.com/NpoolPlatform/chain-middleware/pkg/migrator"
 	servicename "github.com/NpoolPlatform/chain-middleware/pkg/servicename"
 
 	mysqlconst "github.com/NpoolPlatform/go-service-framework/pkg/mysql/const"
@@ -43,6 +45,9 @@ func Init() error {
 	err = db.Init()
 	if err != nil {
 		return fmt.Errorf("cannot init database: %v", err)
+	}
+	if err := migrator.Migrate(context.Background()); err != nil {
+		return fmt.Errorf("fail migrate db: %v", err)
 	}
 
 	return nil
