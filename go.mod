2933c1634368f8f579a7dd77ad6ac99c9e72ecbe
Zhao KK <kikakkz@hotmail.com>
2024-01-03 09:35:36 +0000

    Add coin used for

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/NpoolPlatform/basal-middleware v0.0.0-20231015112137-254853c60eec
 	github.com/NpoolPlatform/go-service-framework v0.0.0-20230930023319-e48074d9472e
 	github.com/NpoolPlatform/libent-cruder v0.0.0-20231008051607-f64457cf6ab6
-	github.com/NpoolPlatform/message v0.0.0-20240103091407-12f6dcf145f7
+	github.com/NpoolPlatform/message v0.0.0-20240103092739-c001efaa6617
 	github.com/go-resty/resty/v2 v2.7.0
 	github.com/google/uuid v1.3.0
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.10.0
---
+++ pkg/crud/coin/usedfor/crud.go
@@ -0,0 +1,96 @@
+package coinusedfor
+
+import (
+	"fmt"
+
+	"github.com/NpoolPlatform/chain-middleware/pkg/db/ent"
+	entcoinusedfor "github.com/NpoolPlatform/chain-middleware/pkg/db/ent/coinusedfor"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	types "github.com/NpoolPlatform/message/npool/basetypes/chain/v1"
+
+	"github.com/google/uuid"
+)
+
+type Req struct {
+	CoinTypeID *uuid.UUID
+	UsedFor    *types.CoinUsedFor
+	DeletedAt  *uint32
+}
+
+func CreateSet(c *ent.CoinUsedForCreate, req *Req) *ent.CoinUsedForCreate {
+	if req.CoinTypeID != nil {
+		c.SetCoinTypeID(*req.CoinTypeID)
+	}
+	if req.UsedFor != nil {
+		c.SetUsedFor(req.UsedFor.String())
+	}
+	return c
+}
+
+func UpdateSet(u *ent.CoinUsedForUpdateOne, req *Req) *ent.CoinUsedForUpdateOne {
+	if req.DeletedAt != nil {
+		u.SetDeletedAt(*req.DeletedAt)
+	}
+	return u
+}
+
+type Conds struct {
+	EntID       *cruder.Cond
+	CoinTypeID  *cruder.Cond
+	CoinTypeIDs *cruder.Cond
+	UsedFor     *cruder.Cond
+	UsedFors    *cruder.Cond
+}
+
+func SetQueryConds(q *ent.CoinUsedForQuery, conds *Conds) (*ent.CoinUsedForQuery, error) {
+	if conds.EntID != nil {
+		id, ok := conds.EntID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid entid")
+		}
+		switch conds.EntID.Op {
+		case cruder.EQ:
+			q.Where(entcoinusedfor.EntID(id))
+		default:
+			return nil, fmt.Errorf("invalid usedfor field")
+		}
+	}
+	if conds.CoinTypeID != nil {
+		id, ok := conds.CoinTypeID.Val.(uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid cointypeid")
+		}
+		switch conds.CoinTypeID.Op {
+		case cruder.EQ:
+			q.Where(entcoinusedfor.CoinTypeID(id))
+		default:
+			return nil, fmt.Errorf("invalid usedfor field")
+		}
+	}
+	if conds.CoinTypeIDs != nil {
+		ids, ok := conds.CoinTypeIDs.Val.([]uuid.UUID)
+		if !ok {
+			return nil, fmt.Errorf("invalid cointypeids")
+		}
+		switch conds.CoinTypeIDs.Op {
+		case cruder.IN:
+			q.Where(entcoinusedfor.CoinTypeIDIn(ids...))
+		default:
+			return nil, fmt.Errorf("invalid usedfor field")
+		}
+	}
+	if conds.UsedFor != nil {
+		usedFor, ok := conds.UsedFor.Val.(types.CoinUsedFor)
+		if !ok {
+			return nil, fmt.Errorf("invalid usedfor")
+		}
+		switch conds.UsedFor.Op {
+		case cruder.EQ:
+			q.Where(entcoinusedfor.UsedFor(usedFor.String()))
+		default:
+			return nil, fmt.Errorf("invalid usedfor field")
+		}
+	}
+	q.Where(entcoinusedfor.DeletedAt(0))
+	return q, nil
+}
